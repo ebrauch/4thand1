844c17d18b694ec60ed9af5bbd076b5c56b5c4af
Evan Brauch <evan.brauch@gmail.com>
2016-02-07 11:27:56 -0700

    adding dummy data to factory

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ var app = angular.module('app', [])
 
 app.factory('playerFactory', [function() {
     var hello = 'hi from factory'
+    var wr = {
+        pname: 'Emmanuel Sanders',
+        pos  : 'RWR'
+    }
+    var rb = {
+        pname: 'C.J. Anderson',
+        pos  : 'RB'
+    }
+    var qb = {
+        pname: 'Brock Osweiler',
+        pos  : 'QB'
+    }
     return {hi: hello}
 }])
 
