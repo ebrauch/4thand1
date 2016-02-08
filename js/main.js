31b57486a9819cdf95835d9fed332af11c1dcb12
Evan Brauch <evan.brauch@gmail.com>
2016-02-08 11:29:32 -0700

    added player and defense data in factory

--- js/main.js
+++ js/main.js
@@ -2,21 +2,34 @@ var app = angular.module('app', [])
 
 app.factory('playerFactory', [function() {
     var hello = 'hi from factory'
-    var wr = {
-        pname: 'Emmanuel Sanders',
-        pos  : 'RWR'
+    var player = {
+        fname: 'Emmanuel',
+        lname: 'Sanders',
+        pos  : 'WR',
+        team : 'DEN',
+        sl   : [8, 8],
+        sm   : [],
+        sr   : [0],
+        dl   : [],
+        dm   : [],
+        dr   : [35, 16]
     }
-    var rb = {
-        pname: 'C.J. Anderson',
-        pos  : 'RB'
+    var defense = {
+        team: 'SD',
+        sl  : [0, 3, 7, 4, 3, 0, 14, 5, 0, 0, 3, 0, 8, 0],
+        sm  : [17, 2, 10, 7, 7, 0, 11, 4, 8, 0],
+        sr  : [10, 0, 0, 3, 7, 33],
+        dl  : [16, 0],
+        dm  : [],
+        dr  : [22, 0, 0, 0]
     }
-    var qb = {
-        pname: 'Brock Osweiler',
-        pos  : 'QB'
-    }
-    return {hi: hello}
+    return {hi    : hello,
+           player : player,
+           defense: defense}
 }])
 
 app.controller('analyze-controller', ['$scope', 'playerFactory', function($scope, playerFactory) {
     console.log(playerFactory.hi)
+    console.log(playerFactory.player)
+    console.log(playerFactory.defense)
 }])
