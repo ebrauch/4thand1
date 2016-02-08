aa911e3afa7703fca615bea25e7fb6f88ec46155
Evan Brauch <evan.brauch@gmail.com>
2016-02-08 12:23:29 -0700

    working autocomplete for player search

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ app.factory('playerFactory', [function() {
     var player = {
         fname: 'Emmanuel',
         lname: 'Sanders',
+        name : this.fname + ' ' + this.lname,
         pos  : 'WR',
         team : 'DEN',
         sl   : [8, 8],
@@ -12,8 +13,10 @@ app.factory('playerFactory', [function() {
         sr   : [0],
         dl   : [],
         dm   : [],
-        dr   : [35, 16]
+        dr   : [35, 16],
+        avg  : [8, 0, 0, 0, 0, 25.5]
     }
+    
     var defense = {
         team: 'SD',
         sl  : [0, 3, 7, 4, 3, 0, 14, 5, 0, 0, 3, 0, 8, 0],
@@ -21,7 +24,8 @@ app.factory('playerFactory', [function() {
         sr  : [10, 0, 0, 3, 7, 33],
         dl  : [16, 0],
         dm  : [],
-        dr  : [22, 0, 0, 0]
+        dr  : [22, 0, 0, 0],
+        avg : [7.85, 6.6, 8.83, 8, 0, 5.5]
     }
     return {hi    : hello,
            player : player,
@@ -30,6 +34,6 @@ app.factory('playerFactory', [function() {
 
 app.controller('analyze-controller', ['$scope', 'playerFactory', function($scope, playerFactory) {
     console.log(playerFactory.hi)
-    console.log(playerFactory.player)
-    console.log(playerFactory.defense)
+    console.log(playerFactory.player.avg)
+    console.log(playerFactory.defense.avg)
 }])
