ea6f959c352ae3456ef54e76fe42f22d2520857d
Evan Brauch <evan.brauch@gmail.com>
2016-03-08 21:13:27 -0700

    installed async

--- controllers/playersCtrl.js
+++ controllers/playersCtrl.js
@@ -1,7 +1,7 @@
 var pbp = require('../models/pbp.js');
 
 function getPlayerData(req, res) {
-    pbp.find({bc: req.params.playerID, succ: 'Y', def: 'DEN'}, function (err, player) {
+    pbp.find({bc: req.params.playerID}, function (err, player) {
 //        console.log(player);
         res.send(player);
 })
