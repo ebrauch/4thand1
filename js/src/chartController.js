0caeca577018d042944465abadc3cf2a47c69bb4
Evan Brauch <evan.brauch@gmail.com>
2016-03-11 14:19:09 -0700

    added team color assignment

--- js/src/chartController.js
+++ js/src/chartController.js
@@ -1,6 +1,40 @@
 angular.module('PlayersApp').controller('chartController', ['$scope', chartController]);
 
 function chartController ($scope) {
+    $scope.teamColors = {
+        ARI: "#97233F",
+        ATL: "#A71930",
+        BAL: "#241773",
+        BUF: "#00338D",
+        CAR: "#0085CA",
+        CHI: "#0B162A",
+        CIN: "#FB4F14",
+        CLE: "#FB4F14",
+        DAL: "#002244",
+        DEN: "#FB4F14",
+        DET: "#005A8B",
+        GB : "#203731",
+        HOU: "#A71930",
+        IND: "#002C5F",
+        JAC: "#D7A22A",
+        KC : "#E31837",
+        STL: "#002244",
+        MIA: "#F58220",
+        MIN: "#4F2683",
+        NE : "#002244",
+        NO : "#9F8958",
+        NYG: "#0B2265",
+        NYJ: "#203731",
+        OAK: "#A5ACAF",
+        PHI: "#004953",
+        PIT: "#FFB612",
+        SD : "#0073CF",
+        SF : "#AA0000",
+        SEA: "#69BE28",
+        TB : "#D50A0A",
+        TEN: "#4B92DB",
+        WAS: "#773141"
+    }
     $scope.buildWrChart = function (wr) {
         $(function () {
             setTimeout(function () {
@@ -57,33 +91,6 @@ function chartController ($scope) {
                                     fillColor: 'white'
                                 }
                             },
-                            //    {
-                            //    type: 'pie',
-                            //    name: 'Total Rushing YPG',
-                            //    data: [{
-                            //        name: rb.display.split(' -')[0],
-                            //        y: (rb.leYds + rb.ltYds + rb.lgYds + rb.mdYds + rb.rgYds + rb.rtYds + rb.reYds) / 16,
-                            //        color: Highcharts.getOptions().colors[0]
-                            //    }, {
-                            //        name: rb.defense,
-                            //        y: (rb.defLeYds + rb.defLtYds + rb.defLgYds + rb.defMdYds + rb.defRgYds + rb.defRtYds + rb.defReYds) / 16,
-                            //        color: Highcharts.getOptions().colors[1]
-                            //    },
-                            //        {
-                            //        name: 'League Average',
-                            //        y: ($scope.leagueAvgLeYds + $scope.leagueAvgLtYds + $scope.leagueAvgLgYds + $scope.leagueAvgMdYds +
-                            //            $scope.leagueAvgRgYds + $scope.leagueAvgRtYds + $scope.leagueAvgReYds),
-                            //        color: Highcharts.getOptions().colors[2]
-                            //    }
-                            //    ],
-                            //    center: [25, 25],
-                            //    size: 33,
-                            //    showInLegend: false,
-                            //    dataLabels: {
-                            //        enabled: false
-                            //    }
-                            //
-                            //}
                         ]
                     })
             }, 0);
@@ -115,7 +122,8 @@ function chartController ($scope) {
                                     Math.floor(rb.rgYds / rb.rgAtt * 100) / 100,
                                     Math.floor(rb.rtYds / rb.rtAtt * 100) / 100,
                                     Math.floor(rb.reYds / rb.rtAtt * 100) / 100
-                                ]
+                                ],
+                                color: $scope.teamColors[rb.cteam]
                             },
                             {
                                 type: 'column',
@@ -128,7 +136,8 @@ function chartController ($scope) {
                                     Math.floor(rb.defRgYds / rb.defRgAtt * 100) / 100,
                                     Math.floor(rb.defRtYds / rb.defRtAtt * 100) / 100,
                                     Math.floor(rb.defReYds / rb.defRtAtt * 100) / 100
-                                ]
+                                ],
+                                color: $scope.teamColors[rb.defense]
                             },
                             {
                                 type: 'spline',
@@ -148,124 +157,12 @@ function chartController ($scope) {
                                     fillColor: 'white'
                                 }
                             },
-                            //    {
-                            //    type: 'pie',
-                            //    name: 'Total Rushing YPG',
-                            //    data: [{
-                            //        name: rb.display.split(' -')[0],
-                            //        y: (rb.leYds + rb.ltYds + rb.lgYds + rb.mdYds + rb.rgYds + rb.rtYds + rb.reYds) / 16,
-                            //        color: Highcharts.getOptions().colors[0]
-                            //    }, {
-                            //        name: rb.defense,
-                            //        y: (rb.defLeYds + rb.defLtYds + rb.defLgYds + rb.defMdYds + rb.defRgYds + rb.defRtYds + rb.defReYds) / 16,
-                            //        color: Highcharts.getOptions().colors[1]
-                            //    },
-                            //        {
-                            //        name: 'League Average',
-                            //        y: ($scope.leagueAvgLeYds + $scope.leagueAvgLtYds + $scope.leagueAvgLgYds + $scope.leagueAvgMdYds +
-                            //            $scope.leagueAvgRgYds + $scope.leagueAvgRtYds + $scope.leagueAvgReYds),
-                            //        color: Highcharts.getOptions().colors[2]
-                            //    }
-                            //    ],
-                            //    center: [25, 25],
-                            //    size: 33,
-                            //    showInLegend: false,
-                            //    dataLabels: {
-                            //        enabled: false
-                            //    }
-                            //
-                            //}
                         ]
                     })
             }, 0);
         })
     }
-    //$scope.buildQbChart = function(qb) {
-    //    $(function () {
-    //        setTimeout(function(){$('#' + qb.player).highcharts(
-    //            {
-    //                credits: {
-    //                    enabled: false
-    //                },
-    //                title: {
-    //                    text: 'Passing Yards Per Attempt'
-    //                },
-    //                xAxis: {
-    //                    categories: ['Short Left', 'Short Middle', 'Short Right', 'Deep Left', 'Deep Middle', 'Deep Right']
-    //                },
-    //                series: [
-    //                    {
-    //                        type: 'column',
-    //                        name: qb.display.split(' -')[0],
-    //                        data: [
-    //                            Math.floor(qb.slYds / qb.slAtt * 100) / 100,
-    //                            Math.floor(qb.smYds / qb.smAtt * 100) / 100,
-    //                            Math.floor(qb.srYds / qb.srAtt * 100) / 100,
-    //                            Math.floor(qb.dlYds / qb.dlAtt * 100) / 100,
-    //                            Math.floor(qb.dmYds / qb.dmAtt * 100) / 100,
-    //                            Math.floor(qb.drYds / qb.drAtt * 100) / 100,
-    //                        ]
-    //                    },
-    //                    {
-    //                        type: 'column',
-    //                        name: qb.defense,
-    //                        data: [
-    //                            Math.floor(qb.defSlYds / qb.defSlTrg * 100) / 100,
-    //                            Math.floor(qb.defSmYds / qb.defSmTrg * 100) / 100,
-    //                            Math.floor(qb.defSrYds / qb.defSrTrg * 100) / 100,
-    //                            Math.floor(qb.defDlYds / qb.defDlTrg * 100) / 100,
-    //                            Math.floor(qb.defDmYds / qb.defDmTrg * 100) / 100,
-    //                            Math.floor(qb.defDrYds / qb.defDrTrg * 100) / 100
-    //                        ]
-    //                    },
-    //                    {
-    //                        type: 'spline',
-    //                        name: 'League Average',
-    //                        data: [
-    //                            $scope.leagueAvgSlYds,
-    //                            $scope.leagueAvgSmYds,
-    //                            $scope.leagueAvgSrYds,
-    //                            $scope.leagueAvgDlYds,
-    //                            $scope.leagueAvgDmYds,
-    //                            $scope.leagueAvgDrYds
-    //                        ],
-    //                        marker: {
-    //                            lineWidth: 2,
-    //                            lineColor: Highcharts.getOptions().colors[3],
-    //                            fillColor: 'white'
-    //                        }
-    //                    },
-    //                    //    {
-    //                    //    type: 'pie',
-    //                    //    name: 'Total Rushing YPG',
-    //                    //    data: [{
-    //                    //        name: rb.display.split(' -')[0],
-    //                    //        y: (rb.leYds + rb.ltYds + rb.lgYds + rb.mdYds + rb.rgYds + rb.rtYds + rb.reYds) / 16,
-    //                    //        color: Highcharts.getOptions().colors[0]
-    //                    //    }, {
-    //                    //        name: rb.defense,
-    //                    //        y: (rb.defLeYds + rb.defLtYds + rb.defLgYds + rb.defMdYds + rb.defRgYds + rb.defRtYds + rb.defReYds) / 16,
-    //                    //        color: Highcharts.getOptions().colors[1]
-    //                    //    },
-    //                    //        {
-    //                    //        name: 'League Average',
-    //                    //        y: ($scope.leagueAvgLeYds + $scope.leagueAvgLtYds + $scope.leagueAvgLgYds + $scope.leagueAvgMdYds +
-    //                    //            $scope.leagueAvgRgYds + $scope.leagueAvgRtYds + $scope.leagueAvgReYds),
-    //                    //        color: Highcharts.getOptions().colors[2]
-    //                    //    }
-    //                    //    ],
-    //                    //    center: [25, 25],
-    //                    //    size: 33,
-    //                    //    showInLegend: false,
-    //                    //    dataLabels: {
-    //                    //        enabled: false
-    //                    //    }
-    //                    //
-    //                    //}
-    //                ]
-    //            })},0);
-    //    })
-    //}
+
     $scope.buildQbChart = function(qb) {
         $(function() {
             setTimeout(function(){
@@ -279,7 +176,7 @@ function chartController ($scope) {
                    },
 
                    title: {
-                       text: 'Passing Yards Per Attempt',
+                       text: 'Passing Yards Per Attempt'
                        //x: -80
                    },
 
@@ -326,7 +223,8 @@ function chartController ($scope) {
                            Math.floor(qb.slYds / qb.slAtt * 100) / 100,
                            Math.floor(qb.dlYds / qb.dlAtt * 100) / 100
                        ],
-                       pointPlacement: 'on'
+                       pointPlacement: 'on',
+                       color: $scope.teamColors[qb.cteam]
                    }, {
                        name: qb.defense,
                        data: [
@@ -337,7 +235,8 @@ function chartController ($scope) {
                            Math.floor(qb.defSlYds / qb.defSlTrg * 100) / 100,
                            Math.floor(qb.defDlYds / qb.defDlTrg * 100) / 100
                        ],
-                       pointPlacement: 'on'
+                       pointPlacement: 'on',
+                       color: $scope.teamColors[qb.defense]
                    }, {
                        name: 'League Average',
                        data: [
@@ -348,7 +247,8 @@ function chartController ($scope) {
                            $scope.leagueAvgSlYds,
                            $scope.leagueAvgDlYds
                        ],
-                       pointPlacement: 'on'
+                       pointPlacement: 'on',
+                       dashStyle: 'shortdash'
                    }]
                })
             },0)
