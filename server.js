722e815409851939d31d5677109fa625e0acb4e0
Evan Brauch <evan.brauch@gmail.com>
2016-04-02 23:32:58 -0600

    updated server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var playersCtrl = require('./controllers/playersCtrl.js');
 app.get('/api/totals', playersCtrl.getTotals);
 app.get('/api/:player', playersCtrl.addPlayer);
 
-var port = 3000;
+var port = process.env.port || 3000;
 
 app.listen(port, function(){
     console.log('server running on port ' + port);
