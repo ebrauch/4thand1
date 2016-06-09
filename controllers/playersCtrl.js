170abf222cab8b03173bc4f450a9ae5491c94fb4
Evan Brauch <evan.brauch@gmail.com>
2016-06-09 09:22:11 -0600

    fixed negative margin

--- controllers/playersCtrl.js
+++ controllers/playersCtrl.js
@@ -3,12 +3,18 @@ var total = require('../models/schemas.js').total;
 
 function getTotals(req, res) {
     total.find({}, function(err, data){
+        if (err) {
+            return 'Error getting league totals';
+        }
         res.send(data);
     })
 }
 
 function addPlayer(req, res) {
     player.find({player: req.params.player}, function(err, data){
+        if (err) {
+            return 'Error getting player data';
+        }
         res.send(data);
     })
 }
